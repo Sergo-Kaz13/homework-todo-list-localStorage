6b4880659edf30c8328d9de7461d63ba3de32bd6
Sergo_Kaz <wwe619dx@ukr.net>
2022-01-16 19:50:42 +0200

    add form

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 'use strict'
+
+
+const {form} = document.forms;
+
+form.addEventListener('submit', saveNote);
+
+async function saveNote(e) {
+  e.preventDefault();
+  let userDate = JSON.parse(localStorage.getItem('userDate')) || [];
+
+  const formDate = new FormData(form);
+  const values = Object.fromEntries(formDate.entries());
+  userDate.push(JSON.stringify(values));
+  localStorage.setItem('userDate', JSON.stringify(userDate));
+}
+
+console.log(JSON.parse(localStorage.getItem('userDate')));
